15713eec96adda893eac1ac2495b5756e61bad55
vinelimaa <vinicius.damas@hotmail.com>
2023-02-27 11:22:47 -0300

    spring cloud

---
+++ currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/CurrencyExchange.java
@@ -0,0 +1,75 @@
+package com.microservices.currencyexchangeservice;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class CurrencyExchange {
+	
+	@Id
+	private Long id;
+	
+	@Column(name = "currency_from")
+	private String from;
+	
+	@Column(name = "currency_to")
+	private String to;
+	private BigDecimal convertionMultiple;
+	private String environment;
+	
+	public CurrencyExchange() {
+		
+	}
+	
+	public CurrencyExchange(Long id, String from, String to, BigDecimal convertionMultiple) {
+		super();
+		this.id = id;
+		this.from = from;
+		this.to = to;
+		this.convertionMultiple = convertionMultiple;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFrom() {
+		return from;
+	}
+
+	public void setFrom(String from) {
+		this.from = from;
+	}
+
+	public String getTo() {
+		return to;
+	}
+
+	public void setTo(String to) {
+		this.to = to;
+	}
+
+	public BigDecimal getConvertionMultiple() {
+		return convertionMultiple;
+	}
+
+	public void setConvertionMultiple(BigDecimal convertionMultiple) {
+		this.convertionMultiple = convertionMultiple;
+	}
+
+	public String getEnvironment() {
+		return environment;
+	}
+
+	public void setEnvironment(String environment) {
+		this.environment = environment;
+	}
+	
+}
---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into CURRENCY_EXCHANGE (id, currency_from, currency_to, convertion_multiple, environment) values (1001, 'USD', 'INR', 65, '');
+insert into CURRENCY_EXCHANGE (id, currency_from, currency_to, convertion_multiple, environment) values (1002, 'EUR', 'INR', 75, '');
+insert into CURRENCY_EXCHANGE (id, currency_from, currency_to, convertion_multiple, environment) values (1003, 'AUD', 'INR', 25, '');
