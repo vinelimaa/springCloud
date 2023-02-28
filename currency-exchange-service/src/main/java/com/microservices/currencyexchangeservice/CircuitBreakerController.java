8ce59f4309d67084b90e0f50ea4cbe862e51e330
vinelimaa <vinicius.damas@hotmail.com>
2023-02-28 11:51:23 -0300

    Circuit Breaker Features

---
+++ currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -0,0 +1,31 @@
+package com.microservices.currencyexchangeservice;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+
+@RestController
+public class CircuitBreakerController {
+	
+	private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
+
+	@GetMapping("/sample-api")
+//	@Retry(name = "sample-api", fallbackMethod = "hardcodeResponse")
+	@CircuitBreaker(name = "default", fallbackMethod = "hardcodeResponse")
+	public String sampleApi() {
+		logger.info("Sample Api call received");
+		ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8000/some-dummy-url", String.class);
+		
+		return forEntity.getBody();
+	}
+	
+	public String hardcodeResponse(Exception ex) {
+		return "fallback-response";
+	}
+}
