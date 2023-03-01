544fe4739d07367dce2452cd11da66b6f6674941
vinelimaa <vinicius.damas@hotmail.com>
2023-03-01 11:46:37 -0300

    updating project

--- currency-exchange-service/src/main/resources/data.sql
+++ currency-exchange-service/src/main/resources/data.sql
@@ -1,3 +1,9 @@
-insert into CURRENCY_EXCHANGE (id, currency_from, currency_to, convertion_multiple, environment) values (1001, 'USD', 'INR', 65, '');
-insert into CURRENCY_EXCHANGE (id, currency_from, currency_to, convertion_multiple, environment) values (1002, 'EUR', 'INR', 75, '');
-insert into CURRENCY_EXCHANGE (id, currency_from, currency_to, convertion_multiple, environment) values (1003, 'AUD', 'INR', 25, '');
+insert into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,environment)
+values(10001,'USD','INR',65,'');
+insert into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,environment)
+values(10002,'EUR','INR',75,'');
+insert into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,environment)
+values(10003,'AUD','INR',25,'');
